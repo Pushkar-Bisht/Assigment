db1152e2a7b4cb5dccb1dba488aec1eeff48e5d2
Pushkar Bisht <65166318+Pushkar-Bisht@users.noreply.github.com>
2022-04-27 15:55:04 +0530

    Add files via upload

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+// define login form
+LoginForm=new FormGroup({
+  email:new FormControl(''),
+  password:new FormControl('')
+})
+  constructor(private http:HttpClient,private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  login(){
+    this.http.get<any>("http://localhost:3000/Signup").subscribe(res=>{
+      const user=res.find((a:any)=>{
+        return a.email===this.LoginForm.value.email && a.password===this.LoginForm.value.password
+      });
+      if(user){
+        alert("Login Sucess");
+        this.LoginForm.reset();
+        this.router.navigate(['listuser'])
+      }
+      else{
+        alert("user not found")
+      }
+    },err=>{
+      alert("Something wrong")
+    })
+  }
+  
 }
